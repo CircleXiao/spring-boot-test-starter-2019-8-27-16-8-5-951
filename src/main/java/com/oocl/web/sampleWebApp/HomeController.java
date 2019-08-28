b51e63d5dec5ac477f057e5d0bd86e14ee87216b
mmy <mmy_xize@163.com>
2019-08-28 16:16:50 +0800

    实现教程中的test例子

--- src/main/java/com/oocl/web/sampleWebApp/HomeController.java
+++ src/main/java/com/oocl/web/sampleWebApp/HomeController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class HomeController {
 	@RequestMapping("/")
 	public @ResponseBody String greeting() {
-		return "hello world";
+		return "Hello World";
 	}
 
 }
--- src/test/java/com/oocl/web/sampleWebApp/SampleWebAppApplicationTests.java
+++ src/test/java/com/oocl/web/sampleWebApp/SampleWebAppApplicationTests.java
@@ -23,13 +23,15 @@ public class SampleWebAppApplicationTests {
 
 	@Test
     public void shouldReturnDefaultMessage() throws Exception {
-        this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk())
-                .andExpect(content().string(containsString("Hello World")));
+        this.mockMvc.perform(get("/"))
+        .andDo(print())
+        .andExpect(status().isOk())
+        .andExpect(content().string(containsString("Hello World")));
     }
 	
-	@Test
-	public void contextLoads() {
-
-	}
+//	@Test
+//	public void contextLoads() {
+//
+//	}
 
 }
