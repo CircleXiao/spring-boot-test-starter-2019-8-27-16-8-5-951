9d802d212a173ee30ae369e34276d13aaaa9071c
mmy <mmy_xize@163.com>
2019-08-28 16:30:21 +0800

    新增接口方法，返回badRequest

--- src/main/java/com/oocl/web/sampleWebApp/HomeController.java
+++ src/main/java/com/oocl/web/sampleWebApp/HomeController.java
@@ -1,13 +1,21 @@
 package com.oocl.web.sampleWebApp;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Controller
 public class HomeController {
 	@RequestMapping("/")
 	public @ResponseBody String greeting() {
+		return "Hello World";
+	}
+	
+	@RequestMapping("/hello")
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	public @ResponseBody String hello() {
 		return "Hello World";
 	}
 
